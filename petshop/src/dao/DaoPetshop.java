0ec1231c3057f4c5b837ddb63fa972c10e8641ae
Administrador <Administrador@10.10.4.19>
2019-09-17 16:30:14 -0300

    1

---
+++ petshop/src/dao/DaoPetshop.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Petshop;
+/**
+
+ */
+
+
+public class DaoPetshop {
+     public static boolean inserir(Petshop objeto) {
+        String sql = "INSERT INTO petshop (codigo, nome, endereco, numero, avaliacao) VALUES (?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.setString(2, objeto.getNome());
+            ps.setString(3, objeto.getEndereco());
+            ps.setInt(4, objeto.getNumero());
+            ps.setInt(5, objeto.getAvaliacao());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+      public static void main(String[] args) {
+        Petshop objeto = new Petshop();
+        objeto.setCodigo(1);
+        objeto.setNome("Teste");
+        objeto.setEndereco("Teste");
+        objeto.setNumero(99);
+        objeto.setAvaliacao(3);
+        
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+      public static List<Petshop> consultar() {
+        List<Petshop> resultados = new ArrayList<>();
+        //editar o SQL conforme a entidade
+        String sql = "SELECT codigo, nome, endereco, numero, avaliacao FROM petshop";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Petshop objeto = new Petshop();
+                //definir um set para cada atributo da entidade, cuidado com o tipo
+                objeto.setCodigo(rs.getInt("codigo"));
+                objeto.setNome(rs.getString("nome"));
+                objeto.setEndereco(rs.getString("endereco"));
+                objeto.setNumero(rs.getInt("numero"));
+                objeto.setAvaliacao(rs.getInt("avaliacao"));
+                
+                resultados.add(objeto);//não mexa nesse, ele adiciona o objeto na lista
+            }
+            return resultados;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+}
+    public static boolean alterar(Petshop objeto) {
+        String sql = "UPDATE petshop SET nome = ?, endereco = ?, numero = ?, avaliacao = ? WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome()); 
+            ps.setString(2, objeto.getEndereco());
+            ps.setInt(3, objeto.getNumero());
+            ps.setInt(4, objeto.getAvaliacao());
+            ps.setInt(5, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    } 
+    public static boolean excluir(Petshop objeto) {
+        String sql = "DELETE FROM petshop WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+}
